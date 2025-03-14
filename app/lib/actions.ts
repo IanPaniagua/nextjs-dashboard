1202742de99c6c7add79ef3d49d2261db74f5c72
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-14 11:29:05 +0100

    create invoice, db, validation, redirect, revalidatePath close #4 ref. #3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,12 +6,40 @@
 // need to create API endpoints
 //  manually when using Server Actions.
 
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+//  type validation
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), //The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+  //It's usually good practice to store monetary values in cents in your database to eliminate 
+  // JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+ 
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
